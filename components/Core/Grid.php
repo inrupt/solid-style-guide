01830052fceccce8fe2f8d4139a7cdb0d00757b0
tony-morelli <tony@morelli.me>
2019-01-11 18:23:08 -0500

    Grid, Cards, and Variable updates

---
+++ components/Core/Grid.php
@@ -0,0 +1,390 @@
+<!-- components/Core/Grid.php -->
+
+
+<h4>5ths-Based Units Grid</h4>
+
+<section class="pure-units-wrap">
+
+	<div class="grids-unit  pure-g">
+        <div class="grids-unit-width pure-u">1-5</div>
+        <div class="grids-unit-details pure-u-1">
+            <div class="pure-g">
+                <div class="grids-unit-bar pure-u-1-5"></div>
+            </div>
+        </div>
+    </div>
+
+	<div class="grids-unit  pure-g">
+        <div class="grids-unit-width pure-u">2-5</div>
+        <div class="grids-unit-details pure-u-1">
+            <div class="pure-g">
+                <div class="grids-unit-bar pure-u-2-5"></div>
+            </div>
+        </div>
+    </div>    
+
+    <div class="grids-unit  pure-g">
+        <div class="grids-unit-width pure-u">3-5</div>
+        <div class="grids-unit-details pure-u-1">
+            <div class="pure-g">
+                <div class="grids-unit-bar pure-u-3-5"></div>
+            </div>
+        </div>
+    </div>
+
+    <div class="grids-unit  pure-g">
+        <div class="grids-unit-width pure-u">4-5</div>
+        <div class="grids-unit-details pure-u-1">
+            <div class="pure-g">
+                <div class="grids-unit-bar pure-u-4-5"></div>
+            </div>
+        </div>
+    </div>
+
+    <div class="grids-unit  pure-g">
+        <div class="grids-unit-width pure-u">5-5</div>
+        <div class="grids-unit-details pure-u-1">
+            <div class="pure-g">
+                <div class="grids-unit-bar pure-u-1"></div>
+            </div>
+        </div>
+    </div>    
+</section>
+
+
+
+<h4>24ths-Based Units Grid</h4>
+
+<section class="pure-units-wrap">
+
+<div class="grids-unit  pure-g">
+        <div class="grids-unit-width pure-u">1-24</div>
+        <div class="grids-unit-details pure-u-1">
+            <div class="pure-g">
+                <div class="grids-unit-bar pure-u-1-24"></div>
+            </div>
+        </div>
+    </div>
+    <div class="grids-unit  pure-g">
+        <div class="grids-unit-width pure-u">1-12</div>
+        <div class="grids-unit-details pure-u-1">
+            <div class="pure-g">
+                <div class="grids-unit-bar pure-u-1-12"></div>
+            </div>
+        </div>
+    </div>
+    <div class="grids-unit grids-unit-not-reduced pure-g">
+        <div class="grids-unit-width pure-u">2-24</div>
+        <div class="grids-unit-details pure-u-1">
+            <div class="pure-g">
+                <div class="grids-unit-bar pure-u-2-24"></div>
+            </div>
+        </div>
+    </div>
+    <div class="grids-unit grids-unit-not-reduced pure-g">
+        <div class="grids-unit-width pure-u">3-24</div>
+        <div class="grids-unit-details pure-u-1">
+            <div class="pure-g">
+                <div class="grids-unit-bar pure-u-3-24"></div>
+            </div>
+        </div>
+    </div>
+    <div class="grids-unit  pure-g">
+        <div class="grids-unit-width pure-u">1-8</div>
+        <div class="grids-unit-details pure-u-1">
+            <div class="pure-g">
+                <div class="grids-unit-bar pure-u-1-8"></div>
+            </div>
+        </div>
+    </div>
+    <div class="grids-unit grids-unit-not-reduced pure-g">
+        <div class="grids-unit-width pure-u">4-24</div>
+        <div class="grids-unit-details pure-u-1">
+            <div class="pure-g">
+                <div class="grids-unit-bar pure-u-4-24"></div>
+            </div>
+        </div>
+    </div>
+    <div class="grids-unit  pure-g">
+        <div class="grids-unit-width pure-u">1-6</div>
+        <div class="grids-unit-details pure-u-1">
+            <div class="pure-g">
+                <div class="grids-unit-bar pure-u-1-6"></div>
+            </div>
+        </div>
+    </div>
+    <div class="grids-unit  pure-g">
+        <div class="grids-unit-width pure-u">5-24</div>
+        <div class="grids-unit-details pure-u-1">
+            <div class="pure-g">
+                <div class="grids-unit-bar pure-u-5-24"></div>
+            </div>
+        </div>
+    </div>
+    <div class="grids-unit  pure-g">
+        <div class="grids-unit-width pure-u">1-4</div>
+        <div class="grids-unit-details pure-u-1">
+            <div class="pure-g">
+                <div class="grids-unit-bar pure-u-1-4"></div>
+            </div>
+        </div>
+    </div>
+    <div class="grids-unit grids-unit-not-reduced pure-g">
+        <div class="grids-unit-width pure-u">6-24</div>
+        <div class="grids-unit-details pure-u-1">
+            <div class="pure-g">
+                <div class="grids-unit-bar pure-u-6-24"></div>
+            </div>
+        </div>
+    </div>
+    <div class="grids-unit  pure-g">
+        <div class="grids-unit-width pure-u">7-24</div>
+        <div class="grids-unit-details pure-u-1">
+            <div class="pure-g">
+                <div class="grids-unit-bar pure-u-7-24"></div>
+            </div>
+        </div>
+    </div>
+    <div class="grids-unit  pure-g">
+        <div class="grids-unit-width pure-u">1-3</div>
+        <div class="grids-unit-details pure-u-1">
+            <div class="pure-g">
+                <div class="grids-unit-bar pure-u-1-3"></div>
+            </div>
+        </div>
+    </div>
+    <div class="grids-unit grids-unit-not-reduced pure-g">
+        <div class="grids-unit-width pure-u">8-24</div>
+        <div class="grids-unit-details pure-u-1">
+            <div class="pure-g">
+                <div class="grids-unit-bar pure-u-8-24"></div>
+            </div>
+        </div>
+    </div>
+    <div class="grids-unit  pure-g">
+        <div class="grids-unit-width pure-u">3-8</div>
+        <div class="grids-unit-details pure-u-1">
+            <div class="pure-g">
+                <div class="grids-unit-bar pure-u-3-8"></div>
+            </div>
+        </div>
+    </div>
+    <div class="grids-unit grids-unit-not-reduced pure-g">
+        <div class="grids-unit-width pure-u">9-24</div>
+        <div class="grids-unit-details pure-u-1">
+            <div class="pure-g">
+                <div class="grids-unit-bar pure-u-9-24"></div>
+            </div>
+        </div>
+    </div>
+    <div class="grids-unit  pure-g">
+        <div class="grids-unit-width pure-u">5-12</div>
+        <div class="grids-unit-details pure-u-1">
+            <div class="pure-g">
+                <div class="grids-unit-bar pure-u-5-12"></div>
+            </div>
+        </div>
+    </div>
+    <div class="grids-unit grids-unit-not-reduced pure-g">
+        <div class="grids-unit-width pure-u">10-24</div>
+        <div class="grids-unit-details pure-u-1">
+            <div class="pure-g">
+                <div class="grids-unit-bar pure-u-10-24"></div>
+            </div>
+        </div>
+    </div>
+    <div class="grids-unit  pure-g">
+        <div class="grids-unit-width pure-u">11-24</div>
+        <div class="grids-unit-details pure-u-1">
+            <div class="pure-g">
+                <div class="grids-unit-bar pure-u-11-24"></div>
+            </div>
+        </div>
+    </div>
+    <div class="grids-unit  pure-g">
+        <div class="grids-unit-width pure-u">1-2</div>
+        <div class="grids-unit-details pure-u-1">
+            <div class="pure-g">
+                <div class="grids-unit-bar pure-u-1-2"></div>
+            </div>
+        </div>
+    </div>
+    <div class="grids-unit grids-unit-not-reduced pure-g">
+        <div class="grids-unit-width pure-u">12-24</div>
+        <div class="grids-unit-details pure-u-1">
+            <div class="pure-g">
+                <div class="grids-unit-bar pure-u-12-24"></div>
+            </div>
+        </div>
+    </div>
+    <div class="grids-unit  pure-g">
+        <div class="grids-unit-width pure-u">13-24</div>
+        <div class="grids-unit-details pure-u-1">
+            <div class="pure-g">
+                <div class="grids-unit-bar pure-u-13-24"></div>
+            </div>
+        </div>
+    </div>
+    <div class="grids-unit  pure-g">
+        <div class="grids-unit-width pure-u">7-12</div>
+        <div class="grids-unit-details pure-u-1">
+            <div class="pure-g">
+                <div class="grids-unit-bar pure-u-7-12"></div>
+            </div>
+        </div>
+    </div>
+    <div class="grids-unit grids-unit-not-reduced pure-g">
+        <div class="grids-unit-width pure-u">14-24</div>
+        <div class="grids-unit-details pure-u-1">
+            <div class="pure-g">
+                <div class="grids-unit-bar pure-u-14-24"></div>
+            </div>
+        </div>
+    </div>
+    <div class="grids-unit  pure-g">
+        <div class="grids-unit-width pure-u">5-8</div>
+        <div class="grids-unit-details pure-u-1">
+            <div class="pure-g">
+                <div class="grids-unit-bar pure-u-5-8"></div>
+            </div>
+        </div>
+    </div>
+    <div class="grids-unit grids-unit-not-reduced pure-g">
+        <div class="grids-unit-width pure-u">15-24</div>
+        <div class="grids-unit-details pure-u-1">
+            <div class="pure-g">
+                <div class="grids-unit-bar pure-u-15-24"></div>
+            </div>
+        </div>
+    </div>
+    <div class="grids-unit  pure-g">
+        <div class="grids-unit-width pure-u">2-3</div>
+        <div class="grids-unit-details pure-u-1">
+            <div class="pure-g">
+                <div class="grids-unit-bar pure-u-2-3"></div>
+            </div>
+        </div>
+    </div>
+    <div class="grids-unit grids-unit-not-reduced pure-g">
+        <div class="grids-unit-width pure-u">16-24</div>
+        <div class="grids-unit-details pure-u-1">
+            <div class="pure-g">
+                <div class="grids-unit-bar pure-u-16-24"></div>
+            </div>
+        </div>
+    </div>
+    <div class="grids-unit  pure-g">
+        <div class="grids-unit-width pure-u">17-24</div>
+        <div class="grids-unit-details pure-u-1">
+            <div class="pure-g">
+                <div class="grids-unit-bar pure-u-17-24"></div>
+            </div>
+        </div>
+    </div>
+    <div class="grids-unit  pure-g">
+        <div class="grids-unit-width pure-u">3-4</div>
+        <div class="grids-unit-details pure-u-1">
+            <div class="pure-g">
+                <div class="grids-unit-bar pure-u-3-4"></div>
+            </div>
+        </div>
+    </div>
+    <div class="grids-unit grids-unit-not-reduced pure-g">
+        <div class="grids-unit-width pure-u">18-24</div>
+        <div class="grids-unit-details pure-u-1">
+            <div class="pure-g">
+                <div class="grids-unit-bar pure-u-18-24"></div>
+            </div>
+        </div>
+    </div>
+    <div class="grids-unit  pure-g">
+        <div class="grids-unit-width pure-u">19-24</div>
+        <div class="grids-unit-details pure-u-1">
+            <div class="pure-g">
+                <div class="grids-unit-bar pure-u-19-24"></div>
+            </div>
+        </div>
+    </div>
+    <div class="grids-unit  pure-g">
+        <div class="grids-unit-width pure-u">5-6</div>
+        <div class="grids-unit-details pure-u-1">
+            <div class="pure-g">
+                <div class="grids-unit-bar pure-u-5-6"></div>
+            </div>
+        </div>
+    </div>
+    <div class="grids-unit grids-unit-not-reduced pure-g">
+        <div class="grids-unit-width pure-u">20-24</div>
+        <div class="grids-unit-details pure-u-1">
+            <div class="pure-g">
+                <div class="grids-unit-bar pure-u-20-24"></div>
+            </div>
+        </div>
+    </div>
+    <div class="grids-unit  pure-g">
+        <div class="grids-unit-width pure-u">7-8</div>
+        <div class="grids-unit-details pure-u-1">
+            <div class="pure-g">
+                <div class="grids-unit-bar pure-u-7-8"></div>
+            </div>
+        </div>
+    </div>
+    <div class="grids-unit grids-unit-not-reduced pure-g">
+        <div class="grids-unit-width pure-u">21-24</div>
+        <div class="grids-unit-details pure-u-1">
+            <div class="pure-g">
+                <div class="grids-unit-bar pure-u-21-24"></div>
+            </div>
+        </div>
+    </div>
+    <div class="grids-unit  pure-g">
+        <div class="grids-unit-width pure-u">11-12</div>
+        <div class="grids-unit-details pure-u-1">
+            <div class="pure-g">
+                <div class="grids-unit-bar pure-u-11-12"></div>
+            </div>
+        </div>
+    </div>
+    <div class="grids-unit grids-unit-not-reduced pure-g">
+        <div class="grids-unit-width pure-u">22-24</div>
+        <div class="grids-unit-details pure-u-1">
+            <div class="pure-g">
+                <div class="grids-unit-bar pure-u-22-24"></div>
+            </div>
+        </div>
+    </div>
+    <div class="grids-unit  pure-g">
+        <div class="grids-unit-width pure-u">23-24</div>
+        <div class="grids-unit-details pure-u-1">
+            <div class="pure-g">
+                <div class="grids-unit-bar pure-u-23-24"></div>
+            </div>
+        </div>
+    </div>
+    <div class="grids-unit  pure-g">
+        <div class="grids-unit-width pure-u">1</div>
+        <div class="grids-unit-details pure-u-1">
+            <div class="pure-g">
+                <div class="grids-unit-bar pure-u-1"></div>
+            </div>
+        </div>
+    </div>
+    <div class="grids-unit  pure-g">
+        <div class="grids-unit-width pure-u">1-1</div>
+        <div class="grids-unit-details pure-u-1">
+            <div class="pure-g">
+                <div class="grids-unit-bar pure-u-1-1"></div>
+            </div>
+        </div>
+    </div>
+    <div class="grids-unit grids-unit-not-reduced pure-g">
+        <div class="grids-unit-width pure-u">24-24</div>
+        <div class="grids-unit-details pure-u-1">
+            <div class="pure-g">
+                <div class="grids-unit-bar pure-u-24-24"></div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
