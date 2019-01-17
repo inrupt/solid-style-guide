0f5954ca86225478f344880d9356bebb60c90be0
tony-morelli <tony@morelli.me>
2019-01-17 18:00:36 -0500

    Typography & Checkbox

--- components/Atoms/Buttons.php
+++ components/Atoms/Buttons.php
@@ -1,6 +1,11 @@
 <!-- components/Atoms/Buttons.php -->
 
+<h4>Default Buttons</h4>
+
 <button type="button">Default</button>
 <button type="button" hover>Hover</button>
 <button type="button" active>Active</button>
 <button type="button" disabled>Disabled</button>
+
+<a class="ids-link">test</a>
+<a class="ids-button" active>test 2</a>
--- components/Core/Typography.php
+++ components/Core/Typography.php
@@ -1,27 +1,52 @@
 <!-- components/Core/Typography.php -->
 
+<div class="grid">
+	<section class="text-raleway card">
+		<h5 class="text-raleway__title">Raleway</h5>
+		<p class="text-raleway__sub-title">Primary Typography - Regular, Bold </p>
+		<br/>
 
-<h4>Primary Typography</h4>
+		<p>Aa Bb Cc Dd Ee Ff 1 2 3 4 5 6 7 8 9 0 ” @ $ ? !</p>
+		<p class="text-raleway--bold-text">Aa Bb Cc Dd Ee Ff 1 2 3 4 5 6 7 8 9 0 ” @ $ ? !</p>
+	</section>
+
+	<section class="text-roboto card">
+		<h5 class="text-roboto__title">Roboto</h5>
+		<p class="text-roboto__sub-title">Secondary Typography - Light, Regular, Bold</p>
+		<br/>
+		<p class="text-roboto--light-text">Aa Bb Cc Dd Ee Ff 1 2 3 4 5 6 7 8 9 0 ” @ $ ? !</p>
+		<p>Aa Bb Cc Dd Ee Ff 1 2 3 4 5 6 7 8 9 0 ” @ $ ? !</p>
+		<p class="text-raleway--bold-text">Aa Bb Cc Dd Ee Ff 1 2 3 4 5 6 7 8 9 0 ” @ $ ? !</p>
+	</section>
+</div>
 
 
-<section class="text-raleway">
-	<h5 class="text-raleway__title">Raleway</h5>
-	<p class="text-raleway__sub-title">Regular, Bold</p>
-	<br/>
-
-	<p>Aa Bb Cc Dd Ee Ff 1 2 3 4 5 6 7 8 9 0 ” @ $ ? !</p>
-	<p class="text-raleway--bold-text">Aa Bb Cc Dd Ee Ff 1 2 3 4 5 6 7 8 9 0 ” @ $ ? !</p>
-</section>
+<h4>Example Typography Styles</h4>
 
 
-<br/>
-<h4>Secondary Typography</h4>
 
-<section class="text-roboto">
-	<h5 class="text-roboto__title">Roboto</h5>
-	<p class="text-roboto__sub-title">Light, Regular, Bold</p>
-	<br/>
-	<p class="text-roboto--light-text">Aa Bb Cc Dd Ee Ff 1 2 3 4 5 6 7 8 9 0 ” @ $ ? !</p>
-	<p>Aa Bb Cc Dd Ee Ff 1 2 3 4 5 6 7 8 9 0 ” @ $ ? !</p>
-	<p class="text-raleway--bold-text">Aa Bb Cc Dd Ee Ff 1 2 3 4 5 6 7 8 9 0 ” @ $ ? !</p>
-</section>
+<h1>h1 Heading - Raleway - Bold</h1>
+<h2>h2 Heading - Raleway - Light</h2>
+<h3>h3 Heading - Roboto - Light</h3>
+<h4>h4 Heading - Raleway - Bold</h4>
+<h5>h5 Heading - Raleway - Bold </h5>
+<h6>h5 Heading - Roboto - Bold</h6>
+
+
+<p>lorem ipsum dolor sit amet. <a href="">This is a Link.</a></p>
+
+<p>lorem <em>ipsum</em> dolor <b>sit</b> amet.</p>
+
+<ul>
+    <li>lorem ipsum unordered list item.</li>
+    <li>lorem ipsum unordered list item.</li>
+    <li>lorem ipsum unordered list item.</li>
+</ul>
+
+<ol>
+    <li>lorem ipsum ordered list item.</li>
+    <li>lorem ipsum ordered list item.</li>
+    <li>lorem ipsum ordered list item.</li>
+</ol>
+
+<small>this is small text.</small>
