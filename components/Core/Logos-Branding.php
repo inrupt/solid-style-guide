73ab9900850abfca122b48589d4d7dfabf175e0a
tony-morelli <tony@morelli.me>
2019-02-04 12:44:12 -0500

    Solid Logo & Link to Github Fixes
    Added SVG logos for inrupt and added examples in the "Logos-branding" section. Also added a direct link to our style guide repository in github

--- components/Core/Logos-Branding.php
+++ components/Core/Logos-Branding.php
@@ -7,3 +7,12 @@
 	<div class="logo logo__item"><img src="/atomic-core/img/inrupt.svg" /></div>
 	<div class="logo logo__item--primary-color"><img src="/atomic-core/img/inrupt-white.svg" /></div>
 </section>
+
+
+
+<h4>Solid</h4>
+
+<section class="logos">
+	<div class="logo logo__item"><img class="logo-small" src="/atomic-core/img/solid.svg" /></div>
+	<div class="logo logo__item--primary-color"><img class="logo-small" src="/atomic-core/img/solid-white.svg" /></div>
+</section>
