9bb02776a8d4d78bc388c56227d7c7f52fc0c841
tony-morelli <tony@morelli.me>
2019-02-13 14:27:35 -0500

    bug/Read-only testing
    Read only updates required additional declarations to override default state and interactivity.

--- components/Atoms/Input.php
+++ components/Atoms/Input.php
@@ -9,7 +9,8 @@
 		<div class="input-wrap"><label>Label:</label><input type="text" placeholder="Placeholder Text" invalid/></div>
 	</section>
 	<section>
-		<div class="input-wrap input-wrap--inline"><label><span class="icon fa fa-lock"></span></label><input type="text" placeholder="Placeholder Text" read-only/></div>
+		<div class="input-wrap input-wrap--inline"><label><span class="icon fa fa-lock"></span></label><input class="read-only" type="text" placeholder="Placeholder Text" read-only/></div>
+		<div class="input-wrap input-wrap--inline"><label><span class="icon fa fa-lock"></span></label><input  type="text" placeholder="Placeholder Text"/></div>
 		<div class="input-wrap input-wrap--inline"><label><span class="icon fa fa-user"></span></label><input type="text" placeholder="Placeholder Text" active/></div>
 		<div class="input-wrap input-wrap--inline"><label><span class="icon fa fa-file"></span></label><input type="text" placeholder="Placeholder Text" invalid/></div>
 	</section>
