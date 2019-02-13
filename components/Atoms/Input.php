ad2b9e9090e354ce74977cc049f3fbd04d3400d8
tony-morelli <tony@morelli.me>
2019-02-13 14:14:02 -0500

    bug/Fix for input states and wrappers
    read only and inline lable/input bug fixes

--- components/Atoms/Input.php
+++ components/Atoms/Input.php
@@ -2,9 +2,15 @@
 
 
 <h4>Text Input</h4>
-
-<div class="input-wrap"><label>Label:</label><input type="text" placeholder="First Name"/></div>
-<div class="input-wrap"><label>Label:</label><input type="text" placeholder="Placeholder Text" active/></div>
-<div class="input-wrap"><label>Label:</label><input type="text" placeholder="Placeholder Text" invalid/></div>
-
-
+<section class="grid grid__two-column demo">
+	<section>
+		<div class="input-wrap"><label>Label:</label><input type="text" placeholder="First Name"/></div>
+		<div class="input-wrap"><label>Label:</label><input type="text" placeholder="Placeholder Text" active/></div>
+		<div class="input-wrap"><label>Label:</label><input type="text" placeholder="Placeholder Text" invalid/></div>
+	</section>
+	<section>
+		<div class="input-wrap input-wrap--inline"><label><span class="icon fa fa-lock"></span></label><input type="text" placeholder="Placeholder Text" read-only/></div>
+		<div class="input-wrap input-wrap--inline"><label><span class="icon fa fa-user"></span></label><input type="text" placeholder="Placeholder Text" active/></div>
+		<div class="input-wrap input-wrap--inline"><label><span class="icon fa fa-file"></span></label><input type="text" placeholder="Placeholder Text" invalid/></div>
+	</section>
+</section>
