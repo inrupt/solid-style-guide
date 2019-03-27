7bef8f2570b87db8f71d92798f48d7bde7a585ee
tony-morelli <tony.morelli@janeirodigital.com>
2019-03-27 10:38:23 -0400

    Bug Fix - Read-Only Inputs
    The select wrap::after/before that provides the dropdown icon requires the select wrap class to include a read-only class as well as the <select> tag.

--- components/Atoms/Select.php
+++ components/Atoms/Select.php
@@ -14,3 +14,13 @@
 
 
 
+<div class="input-wrap"><label>Label:</label>
+	<div class="select-wrap custom-select read-only"><select class="read-only" type="dropdown" placeholder="Please choose..." >
+		<option value="1" name="group1" disabled>Please choose...</option>
+		<option value="1" name="group1" >Alabama</option>
+		<option value="1" name="group1" >Alaska</option>
+		<option value="1" name="group1" >Arizona</option>
+		<option value="1" name="group1" >Arkansas</option>
+	</select></div>
+</div>
+
